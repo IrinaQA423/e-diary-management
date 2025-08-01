9ee1deaa8c82724d0a9c5799800c9f9649823c1e
IrinaQA423 <irina-koshina@mail.ru>
2025-08-01 10:19:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# `scripts.py` — управление электронным дневником через `shell`
+
+Модуль `scripts.py` содержит 3 функции для внесения изменений  в базу  данных электронного дневника.
+
+## Функции
+
+1. `fix_marks(schoolkid)` - заменяет все оценки 2 и 3 у заданного ученика на 5.
+
+2. `remove_chastisements(schoolkid)`- удаляет все замечания у заданного ученика.
+
+3. `create_commendation(schoolkid_name, subject)`- добавляет случайную похвалу для заданного ученика по предмету (cписок записей для похвалы хранится в `COMMENDATIONS` внутри `scripts.py`).
+
+## Как использовать
+
+1. Запустите Django shell.
+
+```
+python manage.py shell
+```
+
+2. Импортируйте функции:
+
+```
+from scripts import fix_marks, remove_chastisements, create_commendation
+from datacenter.models import Schoolkid, Lesson, Commendation, Subject, Mark, Chastisement
+```
+3. Найдите ученика. Например, Фрoлов Иван:
+
+```
+schoolkid = Schoolkid.objects.get(full_name__contains='Фролов Иван')
+```
+
+4. Используйте функции:
+```
+fix_marks(schoolkid)
+remove_chastisements(schoolkid)
+create_commendation('Фролов Иван', 'Музыка')
+```
+
+## Цели проекта
+
+Код написан в учебных целях — это урок в курсе по Python и веб-разработке на сайте [Devman](https://dvmn.org).
---
+++ scripts.py
@@ -0,0 +1,68 @@
+from datacenter.models import Schoolkid, Lesson, Commendation, Subject, Mark, Chastisement
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+import random
+
+COMMENDATIONS = [
+    'Молодец!',
+    'Отлично!',
+    'Хорошо!',
+    'Ты меня приятно удивил!',
+    'Великолепно!',
+    'Прекрасно!',
+    'Ты меня очень обрадовал!',
+    'Очень хороший ответ!',
+    'Талантливо!',
+    'Потрясающе!',
+    'Замечательно!',
+    'Так держать!',
+    'Здорово!',
+    'Я тобой горжусь!',
+    'С каждым разом у тебя получается всё лучше!'
+    'Я вижу, как ты стараешься!',
+    'Ты растешь над собой!'
+]
+
+
+def fix_marks(schoolkid):
+
+    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+    bad_marks.update(points=5)
+    print(f"{bad_marks.count()} плохих оценок исправлено на пятёрки.")
+
+
+def remove_chastisements(schoolkid):
+
+    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+    count = chastisements.count()
+    chastisements.delete()
+    print(f"Удалено {count} замечаний")
+
+
+def create_commendation(schoolkid, subject):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid)
+    except ObjectDoesNotExist:
+        print(f"Ученик с именем '{schoolkid}' не найден")
+        return
+    except MultipleObjectsReturned:
+        print(f"Найдено несколько учеников с именем '{schoolkid}'. Уточните ФИО")
+        return
+
+    lesson = Lesson.objects.filter(
+        subject__title=subject,
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter
+    ).order_by('-date').first()
+
+    if not lesson:
+        print("Уроки по такому предмету не найдены.")
+        return
+
+    Commendation.objects.create(
+        text=random.choice(COMMENDATIONS),
+        created=lesson.date,
+        schoolkid=schoolkid,
+        subject=lesson.subject,
+        teacher=lesson.teacher
+        )
+    print(f"Похвала для {schoolkid} по предмету {subject} добавлена.")
